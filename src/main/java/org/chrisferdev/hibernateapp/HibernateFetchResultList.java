c276a1b33b982752afe17a76756c145f74f0d3e6
Christian <ferchris82@hotmail.com>
2024-02-15 14:33:56 -0500

    Versión 1.0

---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateFetchResultList.java
@@ -0,0 +1,19 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.chrisferdev.hibernateapp.entity.Cliente;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+public class HibernateFetchResultList {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+        List<Cliente> clientes = em.createQuery("SELECT DISTINCT c FROM Cliente c LEFT OUTER JOIN FETCH c.direcciones LEFT OUTER JOIN FETCH c.detalle", Cliente.class).getResultList();
+
+        clientes.forEach(c -> System.out.println(c.getNombre() + ", direcciones: " + c.getDirecciones()));
+        em.close();
+    }
+}
