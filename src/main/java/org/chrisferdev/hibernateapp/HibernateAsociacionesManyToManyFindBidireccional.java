b85fb3471c679330bbdc318290b393e711c8e7d5
Christian <ferchris82@hotmail.com>
2024-02-14 20:45:49 -0500

    Versión 1.0

---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateAsociacionesManyToManyFindBidireccional.java
@@ -0,0 +1,45 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.chrisferdev.hibernateapp.entity.Alumno;
+import org.chrisferdev.hibernateapp.entity.Curso;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+
+public class HibernateAsociacionesManyToManyFindBidireccional {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try{
+            em.getTransaction().begin();
+            Alumno alumno1 = em.find(Alumno.class, 1L);
+            Alumno alumno2 = em.find(Alumno.class, 2L);
+
+            Curso curso1 = em.find(Curso.class, 1L);//new Curso("Curso Java", "Andres");
+            Curso curso2 = em.find(Curso.class, 2L);//new Curso("Curso Hibernate y Jpa", "Andres");
+
+            alumno1.addCurso(curso1);
+            alumno1.addCurso(curso2);
+
+            alumno2.addCurso(curso1);
+
+            em.getTransaction().commit();
+
+            System.out.println(alumno1);
+            System.out.println(alumno2);
+
+            em.getTransaction().begin();
+            // Curso c2 = em.find(Curso.class, 3L);
+            Curso c2 = new Curso("Curso Java", "Andres");
+            c2.setId(2L);
+            alumno1.removeCurso(c2);
+            em.getTransaction().commit();
+            System.out.println(alumno1);
+        }catch (Exception e){
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }finally{
+            em.close();
+        }
+    }
+}
--- src/main/java/org/chrisferdev/hibernateapp/entity/Curso.java
+++ src/main/java/org/chrisferdev/hibernateapp/entity/Curso.java
@@ -3,6 +3,10 @@ package org.chrisferdev.hibernateapp.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Entity
 @Table(name="cursos")
 public class Curso {
@@ -13,12 +17,17 @@ public class Curso {
     private String titulo;
     private String profesor;
 
+    @ManyToMany(mappedBy = "cursos")
+    private List<Alumno> alumnos;
+
     public Curso(String titulo, String profesor) {
+        this();
         this.titulo = titulo;
         this.profesor = profesor;
     }
 
     public Curso() {
+        this.alumnos = new ArrayList<>();
     }
 
     public Long getId() {
@@ -43,6 +52,27 @@ public class Curso {
 
     public void setProfesor(String profesor) {
         this.profesor = profesor;
+    }
+
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(List<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return Objects.equals(id, curso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
