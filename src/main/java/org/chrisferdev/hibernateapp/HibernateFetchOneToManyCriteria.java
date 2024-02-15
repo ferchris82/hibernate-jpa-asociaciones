f4bb50f7b1d752b87183633743ec397d21700387
Christian <ferchris82@hotmail.com>
2024-02-15 16:59:35 -0500

    Versión 1.0

---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateFetchOneToManyCriteria.java
@@ -0,0 +1,32 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
+import org.chrisferdev.hibernateapp.entity.Cliente;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+import org.hibernate.Criteria;
+
+import java.util.List;
+
+public class HibernateFetchOneToManyCriteria {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Cliente> query = cb.createQuery(Cliente.class);
+        Root<Cliente> cliente = query.from(Cliente.class);
+
+        cliente.fetch("direcciones", JoinType.LEFT);
+        cliente.fetch("detalle", JoinType.LEFT);
+        query.select(cliente).distinct(true);
+        List<Cliente> clientes = em.createQuery(query).getResultList();
+
+        clientes.forEach(c -> System.out.println(c.getNombre() + ", detalle: " + c.getDetalle()
+                + ", direcciones: " + c.getDirecciones()));
+        em.close();
+    }
+}
