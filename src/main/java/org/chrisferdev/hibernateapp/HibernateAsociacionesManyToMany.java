1f8ffd3debcfa1b9794d704d33d496b58d8bdff2
Christian <ferchris82@hotmail.com>
2024-02-14 20:01:10 -0500

    Versión 1.0

---
+++ src/main/java/org/chrisferdev/hibernateapp/HibernateAsociacionesManyToMany.java
@@ -0,0 +1,40 @@
+package org.chrisferdev.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.chrisferdev.hibernateapp.entity.Alumno;
+import org.chrisferdev.hibernateapp.entity.Curso;
+import org.chrisferdev.hibernateapp.util.JpaUtil;
+
+public class HibernateAsociacionesManyToMany {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try{
+            em.getTransaction().begin();
+            Alumno alumno1 = new Alumno("Cata", "Edu");
+            Alumno alumno2 = new Alumno("Jano", "Fernan");
+
+            Curso curso1 = new Curso("Curso Java", "Andres");
+            Curso curso2 = new Curso("Curso Hibernate y Jpa", "Andres");
+
+            alumno1.getCursos().add(curso1);
+            alumno1.getCursos().add(curso2);
+
+            alumno2.getCursos().add(curso1);
+
+            em.persist(alumno1);
+            em.persist(alumno2);
+
+            em.getTransaction().commit();
+
+            System.out.println(alumno1);
+            System.out.println(alumno2);
+        }catch (Exception e){
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }finally{
+            em.close();
+        }
+    }
+}
